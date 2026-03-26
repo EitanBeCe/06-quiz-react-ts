2555f6dd7aea55f65d1de2374da1ba7e6dd7e247
Eitan Elberg <eitan.elberg@consumrz.com>
2026-03-26 13:49:56 +0200

    Quiz Completed

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { QUESTIONS } from "../questions.js"
 import { Answer } from "../models/QuestionCodable.js"
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 // type Props = {}
 
@@ -12,22 +13,38 @@ const Quiz = () =>
     const activeQuestionIndex = userAnswers.length
     const i = activeQuestionIndex
 
+    const isQuizComplete = activeQuestionIndex === QUESTIONS.length
+
     const handleSelectedAnswer = (selectedAnswer: Answer) => {
       setUserAnswers(prev => [...prev, selectedAnswer])
     }
 
+    if (isQuizComplete) {
+      return (
+        <div id="summary">
+          <img src={quizCompleteImg} alt="Trophy icon" />
+          <h2>Quiz Completed!</h2>
+        </div>
+      )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[i].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5)
+
     return (
-      <div className="question">
-        <h2>{QUESTIONS[i].text}</h2>
-        <ul id="answers">
-          {QUESTIONS[i].answers.map(answer => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectedAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
+      <div id="quiz">
+        <div id="question">
+          <h2>{QUESTIONS[i].text}</h2>
+          <ul id="answers">
+            {shuffledAnswers.map(answer => (
+              <li key={answer} className="answer">
+                <button onClick={() => handleSelectedAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     )
   }
